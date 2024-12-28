f780ecf57804e10d23dadaba1ebba8e4a9898258
rishabh-grade <risabh.grade28@gmail.com>
2024-12-28 23:42:46 +0530

    Update db.js

--- src/db/db.js
+++ src/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.connectDB)
+        await mongoose.connect(process.env.DB_STRING)
         console.log("mongodb connected!!")
     } catch (err) {
         console.log(err)
